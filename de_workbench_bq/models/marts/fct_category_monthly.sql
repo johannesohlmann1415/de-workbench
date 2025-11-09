0310a74ca9d8f28c6d7d176f5829938b246da488
johannesohlmann1415 <johannes.ohlmann@gmail.com>
2025-11-09 12:30:12 +0100

    mart3

--- de_workbench_bq/models/marts/fct_category_monthly.sql
+++ de_workbench_bq/models/marts/fct_category_monthly.sql
@@ -1,41 +1,66 @@
-{{ config(materialized='incremental',
-    unique_key = 'order_id',
-    partition_by={
-      "field": "order_date",
-      "data_type": "date",
-      "granularity": "day"
-    },
-    cluster_by = ['customer_id', 'status'],
-    incremental_strategy="merge",
-    merge_update_columns = ['status', 'shipping_price', 'payment_method', 'items_revenue', 'order_total_calc']
-    ) }}
-With items as (
+{{ config(
+  materialized='incremental',
+  incremental_strategy='merge',
+  unique_key=['month','category'],
+  partition_by={
+    "field": "month",
+    "data_type": "date",
+    "granularity": "month"
+  },
+  cluster_by=['category']
+) }}
+
+with base_cte as (
   Select
-    order_id,
-    SUM(i.quantity*i.unit_price) as items_revenue,
-  From {{ ref('stg_order_items') }} as i
-  Group by order_id
+  DATE_TRUNC(fo.order_date, MONTH) as month,
+  sp.category as category,
+  fo.order_id as order_id,
+  so.quantity as quantity,
+  sp.product_id,
+  so.unit_price as unit_price,
+  From {{ ref('fct_orders') }} as fo
+  left join {{ ref('stg_order_items') }} as so using(order_id)
+  left join {{ ref('stg_products_ext') }} sp using(product_id)
+  Where fo.status = 'completed'
+  {% if is_incremental() %}
+    and DATE_TRUNC(fo.order_date, MONTH) >= (
+      SELECT DATE_SUB(COALESCE(MAX(month), DATE '1900-01-01'), INTERVAL 1 MONTH)
+      FROM {{ this }}
+    )
+  {% endif %}
+),
+prod_rev as (
+    Select
+    month,
+    category,
+    product_id,
+    Sum(quantity*unit_price) as prod_revenue,
+    From base_cte
+    Group by 1,2,3
+),
+top_prod as(
+    Select
+    month,
+    category,
+    product_id as top_product,
+    prod_revenue,
+    From prod_rev
+    Qualify Row_Number() over(partition by month, category order by prod_revenue desc, product_id asc) = 1
+),
+
+month_cat as (
+    Select
+    month,
+    category,
+    Count(Distinct(order_id)) as orders_cnt,
+    SUM(quantity*unit_price) as revenue,
+    from base_cte
+    Group by 1,2
 )
+
 Select
-  o.order_id, 
-  o.customer_id, 
-  o.order_date,
-  o.status,
-  o.total_amount,
-  o.coupon_count, 
-  o.first_coupon, 
-  o.device, 
-  o.payment_method, 
-  o.shipping_method,
-  o.shipping_price,
-  CASE WHEN status = "completed" then True else False End as is_completed,
-  COALESCE(i.items_revenue, 0) as items_revenue,
-  COALESCE(shipping_price + items_revenue, 0) as order_total_calc
-From {{ ref('stg_orders_ext') }} as o
-left join items as i using(order_id)
-{% if is_incremental() %}
-  WHERE order_date >= (
-    SELECT DATE_SUB(COALESCE(MAX(order_date), DATE '1900-01-01'), INTERVAL 2 DAY)
-    FROM {{ this }}
-  )
-{% endif %}
+  m.*,
+  tp.top_product,
+  tp.prod_revenue
+From month_cat as m
+left join top_prod as tp using(month, category)
